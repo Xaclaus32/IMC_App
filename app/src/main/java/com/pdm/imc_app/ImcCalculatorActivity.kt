ae4b4d794eab7c3cde33d5089e138fd2c8346996
Xavi <xaclaus.32@gmail.com>
2023-11-08 11:36:02 +0100

    v0.1

---
+++ app/src/main/java/com/pdm/imc_app/ImcCalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.pdm.imc_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imccalculatoractivity)
+    }
+}
